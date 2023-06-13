3a85e8b4b9aebe1e873442711f7250b278e098fb
Anekant <Anekantjainsagar@gmail.com>
2023-06-14 00:40:05 +0530

    Initial frontend commit

---
+++ src/Utils/index.js
@@ -0,0 +1,5 @@
+export const SOCKET_URI = "https://sociopathsocketserver.onrender.com";
+export const LOGIN_URI = "https://sociopathbackend.onrender.com/login";
+export const BACKEND_URI = "https://sociopathbackend.onrender.com";
+export const POST_URI = "https://sociopathbackend.onrender.com/post";
+export const QUERY_URI = "https://sociopathbackend.onrender.com/query";
